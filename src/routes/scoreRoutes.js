adc1a3510b0723f777a6f62b890aa19f388c7223
thienhuong320 <phantranthienhuong320@gmail.com>
2024-11-09 01:20:18 +0700

    update scoreRouter

--- src/routes/scoreRoutes.js
+++ src/routes/scoreRoutes.js
@@ -4,7 +4,7 @@ const scoreController = require("../controllers/scoreController");
 
 // Define routes for scores
 router.get("/", scoreController.getAllScores); // Lấy tất cả score
-router.get("/:id", scoreController.getScoreById); // Lấy score theo id
+router.get("/:score_id", scoreController.getScoreById); // Lấy score theo id
 router.post("/create", scoreController.createScore); // Tạo mới score
 router.put("/update/:id", scoreController.updateScore); // Cập nhật score
 router.delete("/delete/:id", scoreController.deleteScore); // Xóa score
